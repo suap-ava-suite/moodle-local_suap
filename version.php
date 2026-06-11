30c193a6114906df23e80a17784bd3d1d7a01852
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2026-06-11 17:21:52 -0300

    refactor(sync_up_enrolments): [ADD] tratamento de logs na sincronização
    [UPD] renomear variável de controle de sala para coordenação

--- version.php
+++ version.php
@@ -27,8 +27,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_suap';
-$plugin->release = '4.5.109';
-$plugin->version = 20260610109;
+$plugin->release = '4.5.110';
+$plugin->version = 20260611110;
 $plugin->maturity = MATURITY_STABLE;
 
 $plugin->requires = 2021051700;
