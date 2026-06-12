4a3a5aa4e7509e01b77c61590ce22326396e9200
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2026-06-12 17:19:57 -0300

    style: inicio da adoção de ferramenta de code style

--- version.php
+++ version.php
@@ -27,8 +27,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_suap';
-$plugin->release = '4.5.110';
-$plugin->version = 20260611110;
+$plugin->release = '4.5.111';
+$plugin->version = 20260612111;
 $plugin->maturity = MATURITY_STABLE;
 
 $plugin->requires = 2021051700;
